e730b5383c0dc7f49466477259478fcf16bdd0de
Job van de Ven <jobvandeven@live.nl>
2021-02-02 23:24:50 +0100

    Display for all tasks fixed

--- Display.js
+++ Display.js
@@ -1,25 +1,40 @@
 function displayTasks(tasks, tabel) {
-    var tabel = document.getElementById('');
+    var tabel = document.getElementById(tabel);
+    tabel.innerHTML = "";
     var counter = 0;
+    var row;
     tasks.forEach(task => {
         if (counter % 2 == 0) {
-            var row = tabel.insterRow();
+            row = tabel.insertRow(-1);
         }
-        createRadioButton(task.getName(), task.getId(),'taak', row);
+        createRadioButton(task.getName(), task.getId(),'Taakbutton', row);
+        counter++;
+    });
+}
+
+function displaySpecials(specials, tabel){
+    var tabel = document.getElementById(tabel);
+    tabel.innerHTML = "";
+    var counter = 0;
+    var row;
+    specials.forEach(special => {
+        if (counter % 2 == 0) {
+            row = tabel.insertRow(-1);
+        }
+        createRadioButton(special.getName(), special.getId(),'Taakbutton', row);
         counter++;
     });
 }
 
 function createRadioButton(naam, id, name, rij){
-    var rij = document.getElementById(rij);
-    var cell = tabel.insertCell(-1);
+    var cell = rij.insertCell(-1);
     var radio = document.createElement('input');
     radio.type = 'radio';
     radio.name = name;
     radio.id = id;
 
     var label = document.createElement('label');
-    label.htmlFor(id);
+    label.htmlFor = id;
 
     var description = document.createTextNode(naam);
     label.appendChild(description);
